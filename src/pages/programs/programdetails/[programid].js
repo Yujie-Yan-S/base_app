319ecbb693ff91d899028fd48a0fa97121724064
Yujie-Yan-S <cdyyj@bu.edu>
2023-08-09 19:49:58 -0400

    finished the assesment ui

--- src/pages/programs/programdetails/[programid].js
+++ src/pages/programs/programdetails/[programid].js
@@ -7,6 +7,9 @@ import 'slick-carousel/slick/slick-theme.css'
 import HomePageCard from "../../home/components/HomePageCard";
 import React from "react";
 import Divider from "@mui/material/Divider";
+import Button from "@mui/material/Button";
+import {Stack} from "@mui/system";
+import Avatar from "@mui/material/Avatar";
 
 const CourseDetails = () => {
   const router = useRouter();
@@ -52,7 +55,7 @@ const CourseDetails = () => {
         </Typography>
 
       </Box>
-      <Divider sx={{ background: theme =>theme.palette.primary.main, width:"1037px"  }} />
+      <Divider sx={{background: theme => theme.palette.primary.main, width: "1037px"}}/>
       <Box
         sx={{
           width: '1037px',
@@ -95,7 +98,7 @@ const CourseDetails = () => {
 
 
       </Box>
-      <Divider sx={{ background: theme =>theme.palette.primary.main, width:"1037px"  }} />
+      <Divider sx={{background: theme => theme.palette.primary.main, width: "1037px"}}/>
 
       <Box
         sx={{
@@ -111,17 +114,57 @@ const CourseDetails = () => {
 
       </Box>
 
-      <Divider sx={{ background: theme =>theme.palette.primary.main, width:"1037px"  }} />
+      <Divider sx={{background: theme => theme.palette.primary.main, width: "1037px"}}/>
 
 
       <Box
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        flexDirection={"row"}
         sx={{
           width: '100%',
           height: '450px',
           background: 'white',
         }}
       >
+        {/*set the white space*/}
+        <Box sx={{height: "73.333%", width: "85%"}} display={"flex"} alignItems={"center"}>
+          {/*left of assesment*/}
+          <Box sx={{width: "67.4%", height: "100%"}} display={'flex'} flexDirection={"column"}>
+            <Box sx={{height: "10%"}} display={"flex"} justifyContent={"start"} alignItems={"center"}>
+              <Typography variant={"h3"} color={theme => theme.palette.primary.main}>Assesment</Typography>
+            </Box>
+            <Box sx={{height: "73.5%", width: "89%"}} display={"flex"} justifyContent={"start"} alignItems={"center"}>
+              <Typography mt={4} sx={{fontWeight: 600, fontSize: '17px'}}>Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum</Typography>
+            </Box>
+            <Box sx={{height: "16.5%"}}>
+              <Button variant="contained" sx={{height: "55px", width: "360px"}}>
+                <Typography color={"white"} variant={'h5'}>Take The Assesment</Typography>
+              </Button>
+            </Box>
+          </Box>
 
+
+          {/*  right of assesment*/}
+          <Box sx={{width: "32.6%", height: "54%"}} display={"flex"} alignItems={"center"} flexDirection={"column"} justifyContent={"space-between"}>
+            <Typography variant={'h4'}>Certified By...</Typography>
+            <Box>
+              <Stack direction="row" spacing={2}>
+                <Avatar sx={{height:'100px', width:"100px"}}> </Avatar>
+                <Avatar sx={{height:'100px', width:"100px"}}> </Avatar>
+                <Avatar sx={{height:'100px', width:"100px"}}> </Avatar>
+              </Stack>
+            </Box>
+          </Box>
+        </Box>
 
       </Box>
 
