52d9a6bea9d7115ba7e3450c87a9d5e8de5ecb29
Yujie-Yan-S <cdyyj@bu.edu>
2023-08-08 20:51:16 -0400

    make the layout of program details

--- src/pages/programs/programdetails/[programid].js
+++ src/pages/programs/programdetails/[programid].js
@@ -1,4 +1,6 @@
 import { useRouter } from 'next/router';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 
 const CourseDetails = () => {
@@ -8,11 +10,59 @@ const CourseDetails = () => {
   // 在这里可以根据 courseid 加载相应的课程信息
 
   return (
-    <div>
-      <h1>Course Details</h1>
-      <p>Course ID: {programid}</p>
-      {/* 显示课程详细信息 */}
-    </div>
+    <Box>
+    <Box
+      sx={{
+        width:'100%',
+        height:'470px',
+        background:'pink',
+      }}
+    >
+  <Typography
+  variant={'h2'}
+  >
+    banner
+  </Typography>
+
+    </Box>
+      <Box
+        sx={{
+          width:'100%',
+          height:'430px',
+          background:'yellow',
+        }}
+      >
+
+
+      </Box>
+
+      <Box
+        sx={{
+          width:'100%',
+          height:'410px',
+          background:'green',
+        }}
+      >
+        <Typography
+          variant={'h2'}
+        >
+          projects
+        </Typography>
+
+      </Box>
+
+      <Box
+        sx={{
+          width:'100%',
+          height:'450px',
+          background:'white',
+        }}
+      >
+
+
+      </Box>
+
+    </Box>
   );
 };
 
